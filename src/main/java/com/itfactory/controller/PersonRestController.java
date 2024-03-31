1af3b362d1f8a1441b44757c9671fe614290fc2c
radoivlad <radoivlad9@gmail.com>
2024-03-31 22:49:54 +0300

    - removed unused import;

--- src/main/java/com/itfactory/controller/PersonRestController.java
+++ src/main/java/com/itfactory/controller/PersonRestController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
